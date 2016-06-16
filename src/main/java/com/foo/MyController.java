d5f4c4474e170f514c4d4b6a62e574915bc00027
Ravindra Bandara <rbandara@bn.com>
2016-06-16 12:46:46 -0400

    adding java skeleton project

---
+++ src/main/java/com/foo/MyController.java
@@ -0,0 +1,7 @@
+package com.foo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+}
---
+++ src/main/java/com/foo/MyService.java
@@ -0,0 +1,7 @@
+package com.foo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
